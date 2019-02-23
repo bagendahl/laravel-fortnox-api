83c0d8416f0bc1513431d11aca600533af86988a
tarre com <tarre.islam@gmail.com>
2019-02-23 10:18:04 +0100

    kfd

---
+++ src/Contracts/BaseApiRepository.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tarre
+ * Date: 2019-02-20
+ * Time: 22:45
+ */
+
+namespace Tarre\Fortnox\Contracts;
+
+
+interface BaseApiRepository
+{
+    /**
+     * @param $key
+     * @param $value
+     * @return $this
+     */
+    public function setQueryKey($key, $value);
+
+    /**
+     * @param $key
+     * @return mixed
+     */
+    public function filter($key);
+
+    /**
+     * @param $number
+     * @return $this
+     */
+    public function take($number);
+    /**
+     * @param $number
+     * @return $this
+     */
+    public function skip($number);
+    /**
+     * @param $number
+     * @return $this
+     */
+    public function page($number);
+    /**
+     * @param string $column
+     * @param string $sortOrder
+     * @return $this
+     */
+    public function sortBy(string $column, $sortOrder = 'ascending');
+    /**
+     * @param string $column
+     * @param string $sortOrder
+     * @return $this
+     */
+    public function orderBy(string $column, $sortOrder = 'ascending');
+
+    /**
+     * @param array $data
+     * @return $this
+     */
+    public function withRequestOptions(array $data);
+
+
+}
