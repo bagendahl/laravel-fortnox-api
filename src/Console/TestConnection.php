364556c735faad2e0edb7a20c1fe59c0d835587a
Tariqul Islam <tarre.islam@gmail.com>
2020-07-22 23:54:35 +0200

    increased verbosity of logz

--- src/Console/TestConnection.php
+++ src/Console/TestConnection.php
@@ -9,6 +9,8 @@
 namespace Tarre\Fortnox\Console;
 
 
+use Config;
+use Exception;
 use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Console\Command;
 use Tarre\Fortnox\Api\Customers\FortnoxCustomer;
@@ -20,7 +22,7 @@ class TestConnection extends Command
      *
      * @var string
      */
-    protected $signature = 'fortnox:test';
+    protected $signature = 'fortnox:test {seconds=60}';
 
     /**
      * The console command description.
@@ -47,32 +49,40 @@ class TestConnection extends Command
      */
     public function handle(FortnoxCustomer $fortnoxCustomer)
     {
-        $this->info('Testing rate limit (This will take at least 70 seconds)');
+
+        $this->info('[x] Checking config');
 
         $messages = [];
 
-        if (!\Config::has('laravel-fortnox')) {
-            $messages[] = 'Configuration is missing. Publish the configuration';
+        if (!Config::has('laravel-fortnox')) {
+            $messages[] = 'Configuration is missing. Publish the configuration "php artisan vendor:publish --tag="laravel-fortnox"';
+        }else{
+            $this->info('[x] Config ok!');
         }
 
+        $this->info('[x] Testing API connection');
         if (!$fp = @fsockopen(gethostbyname('api.fortnox.se'), 443, $errno, $errStr, 5)) {
             $messages[] = 'Failed to establish HTTPS to the endpoint: ' . $errStr;
         } else {
+            $this->info('[x] API connection ok!');
             fclose($fp);
         }
 
         // Check that the rate limit IS OK
         $numRequests = 0;
         $now = now();
+        $nArgSeconds = intval($this->argument('seconds'));
 
-        while ($now->diffInSeconds() < 70) {
+        $this->info(sprintf('[x] Testing rate limit (This will take at least %d seconds)', $nArgSeconds));
+
+        while ($now->diffInSeconds() < $nArgSeconds) {
             try {
                 $fortnoxCustomer->take(500)->get()
                     ->each(function ($row) use ($fortnoxCustomer, &$numRequests) {
                         $numRequests++;
                         $fortnoxCustomer->getByDocumentNumber($row['CustomerNumber']);
                     });
-            } catch (\Exception $exception) {
+            } catch (Exception $exception) {
                 $messages[] = sprintf($exception->getMessage());
             }
         }
@@ -84,6 +94,7 @@ class TestConnection extends Command
             }
         } else {
             $rps = round($numRequests / $now->diffInSeconds(), 2);
+            $this->info('[x] Rate limit ok!');
             $this->info(sprintf('%d requests made in %d seconds. %s RPS',
                 $numRequests,
                 $now->diffInSeconds(),
